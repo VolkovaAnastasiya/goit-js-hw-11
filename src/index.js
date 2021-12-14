27184d38513a97e6182940108f1564707cb4539e
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2021-12-14 12:14:52 +0200

    Поправляет исчезновения кнопки

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-import axios from 'axios';
+
 import Notiflix from 'notiflix';
 import counterCard from '../src/templates/counterCard.hbs';
 import NewApiService from './new-server';
@@ -21,12 +21,14 @@ function onSearch(e) {
   newApiService.query = e.currentTarget.elements.searchQuery.value.trim();
 
   newApiService.resetPage();
+
   clearHitsMarkup();
   newApiService.fetchArticles().then(data => {
     if (!data.hits.length || newApiService.query === '') {
+      buttonLoadMoreRef.classList.add('is-hidden');
       clearHitsMarkup();
       onFetchError();
-      buttonLoadMoreRef.classList.add('is-hidden');
+
       return;
     }
     appendHitsMarkup(data);
@@ -34,13 +36,14 @@ function onSearch(e) {
   });
 }
 
-function onLoadMore(e) {
+function onLoadMore() {
   newApiService.fetchArticles().then(scrollRender);
 }
 
 function appendHitsMarkup(data) {
-  cardsGalleryRef.insertAdjacentHTML('beforeend', counterCard(data.hits));
   buttonLoadMoreRef.classList.remove('is-hidden');
+  cardsGalleryRef.insertAdjacentHTML('beforeend', counterCard(data.hits));
+
   const lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
     captionsData: 'alt',
@@ -55,8 +58,6 @@ function scrollRender(data) {
     captionsData: 'alt',
   });
 
-  lightbox.refresh();
-
   if (data.hits.length < 40 && data.hits.length >= 0) {
     buttonLoadMoreRef.classList.add('is-hidden');
     Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
@@ -69,13 +70,16 @@ function scrollRender(data) {
     top: cardHeight * 2,
     behavior: 'smooth',
   });
+  lightbox.refresh();
 }
 
 function clearHitsMarkup() {
   cardsGalleryRef.innerHTML = '';
+  buttonLoadMoreRef.classList.add('is-hidden');
 }
 
 function onFetchError(error) {
+  buttonLoadMoreRef.classList.add('is-hidden');
   Notiflix.Notify.failure(
     'Sorry, there are no images matching your search query. Please try again.',
   );
